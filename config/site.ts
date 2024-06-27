e2305d5d6755b11b02780f42f4d22325a8a99aee
wahyudesu <wahyuikbal777@gmail.com>
2024-06-27 16:18:55 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -7,7 +7,8 @@ export const siteConfig = {
     twitter: "https://twitter.com/jollyshopland",
     github: "https://github.com/wahyudesu",
     medium: "https://medium.com/@wahyuikbal",
-    linkedin: "https://www.linkedin.com/in/wahyuikbalmaulana"
+    linkedin: "https://www.linkedin.com/in/wahyuikbalmaulana",
+    personalSite: "https://wahyuikbal.framer.website"
   },
 };
 
