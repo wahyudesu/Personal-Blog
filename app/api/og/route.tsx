96151846389e90af3a2ac8548a6038fe72d30117
wahyudesu <wahyuikbal777@gmail.com>
2024-07-14 20:19:03 +0700

    Update route.tsx

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -5,7 +5,7 @@ const interRegular = fetch(
 ).then((res) => res.arrayBuffer())
 
 const interBold = fetch(
-  new URL("../../../assets/fonts/CalSans-SemiBold.ttf", import.meta.url)
+  new URL("../../../assets/fonts/CalSans-SemiBold.woff", import.meta.url)
 ).then((res) => res.arrayBuffer())
 
 export async function GET(request: Request) {
